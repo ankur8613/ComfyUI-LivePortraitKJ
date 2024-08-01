7fd02ae2c5557bc63d037f5e53134630b4016998
ankur8613 <134399424+ankur8613@users.noreply.github.com>
2024-08-01 20:01:14 +0530

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -110,7 +110,7 @@ class DownloadAndLoadLivePortraitModels:
 
         pbar = comfy.utils.ProgressBar(3)
 
-        download_path = "/home/ubuntu/ComfyUI/custom_nodes/ComfyUI-LivePortraitKJ"
+        download_path = download_path = os.path.join(folder_paths.models_dir, "liveportrait")
         model_path = os.path.join(download_path)
 
         if not os.path.exists(model_path):
